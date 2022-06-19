e37ddae54a90c30aeb8f667ee32297a565359211
jerome <jerome@geekstudio.co.kr>
2022-06-19 13:53:49 +0900

    :sparkles: feat: AppLoading Component 추가 & Font / Image Preload 추가

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+// TODO: expo-app-loading is deprecated, Need To change expo-splash-screen
+import AppLoading from 'expo-app-loading';
+import { Image, Text } from 'react-native';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { Asset } from 'expo-asset';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [ready, setReady] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const onFinish = () => setReady(true);
+
+    const startLoading = async () => {
+        await Font.loadAsync(Ionicons.font);
+        await Asset.loadAsync(require('./instagram.png'));
+        await Image.prefetch('https://reactnative.dev/img/oss_logo.png');
+    };
+
+    if (!ready) {
+        return (
+            <AppLoading
+                startAsync={startLoading}
+                onFinish={onFinish}
+                onError={console.error}
+            />
+        );
+    }
+
+    return <Text>We are done</Text>;
+}
