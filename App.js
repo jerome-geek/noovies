b6e27ecbfe7bb04be01e827bfe0477571c6a39be
jerome <jerome@geekstudio.co.kr>
2022-06-22 23:35:30 +0900

    :sparkles: feat: 앱 로딩시 폰트, 이미지 로딩 추가

--- App.js
+++ App.js
@@ -4,17 +4,39 @@ import AppLoading from 'expo-app-loading';
 import { Image, Text } from 'react-native';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
-import { Asset } from 'expo-asset';
+import { Asset, useAssets } from 'expo-asset';
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+    images.map((image) => {
+        if (typeof image === 'string') {
+            return Image.prefetch(image);
+        } else {
+            return Asset.loadAsync(image);
+        }
+    });
 
 export default function App() {
+    // 단순 asset만 prefetch하려면 hooks를 사용하는 것이 낫다
+    // const [assets] = useAssets([
+    //     require('./instagram.png'),
+    //     'https://reactnative.dev/img/oss_logo.png',
+    // ]);
+    // const [loaded] = Font.useFonts(Ionicons.font);
+
     const [ready, setReady] = useState(false);
 
     const onFinish = () => setReady(true);
 
     const startLoading = async () => {
-        await Font.loadAsync(Ionicons.font);
-        await Asset.loadAsync(require('./instagram.png'));
-        await Image.prefetch('https://reactnative.dev/img/oss_logo.png');
+        const fonts = loadFonts([Ionicons.font]);
+        const images = loadImages([
+            require('./instagram.png'),
+            'https://reactnative.dev/img/oss_logo.png',
+        ]);
+
+        await Promise.all([...fonts, ...images]);
     };
 
     if (!ready) {
