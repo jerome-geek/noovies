86ae4f85120b5e68597214f7543ff8c89a6a2c31
jerome <jerome@geekstudio.co.kr>
2022-06-22 23:36:59 +0900

    :sparkles: feat: bottom navigation 추가
     - react-navigation추가

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { Image, Text } from 'react-native';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { Asset, useAssets } from 'expo-asset';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './navigation/Tabs';
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
@@ -49,5 +51,9 @@ export default function App() {
         );
     }
 
-    return <Text>We are done</Text>;
+    return (
+        <NavigationContainer>
+            <Tabs />
+        </NavigationContainer>
+    );
 }
