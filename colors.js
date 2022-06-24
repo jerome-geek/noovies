9a31c101debc3f576cb6a9f30236ab3d9d549d3d
jerome <jerome@geekstudio.co.kr>
2022-06-24 12:52:34 +0900

    :sparkles: feat: bottom navigation에 color 추가
     - 다크모드 여부에 따라 별도의 색상 추가

---
+++ colors.js
@@ -0,0 +1,5 @@
+export const YELLOW = '#ffa801';
+export const BLACK = '#1e272e';
+export const DARK_GREY = '#d2dae2';
+export const LIGHT_GREY = '#808e9b';
+export const WHITE = '#fff';
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import { useColorScheme } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
+import { BLACK, DARK_GREY, LIGHT_GREY, WHITE, YELLOW } from '../colors';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-    <Tab.Navigator>
-        <Tab.Screen name='Movies' component={Movies} />
-        <Tab.Screen name='Tv' component={Tv} />
-        <Tab.Screen name='Search' component={Search} />
-    </Tab.Navigator>
-);
+// 다크모드 변경 in iOS: command + shift + a
+const Tabs = () => {
+    const isDark = useColorScheme() === 'dark';
+    console.log('🚀 ~ file: Tabs.js ~ line 13 ~ Tabs ~ isDark', isDark);
+
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: isDark ? BLACK : WHITE,
+                },
+                tabBarActiveTintColor: isDark ? YELLOW : BLACK,
+                tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY,
+                headerStyle: {
+                    backgroundColor: isDark ? BLACK : WHITE,
+                },
+                headerTitleStyle: {
+                    color: isDark ? WHITE : BLACK,
+                },
+            }}
+        >
+            <Tab.Screen name='Movies' component={Movies} />
+            <Tab.Screen name='Tv' component={Tv} />
+            <Tab.Screen name='Search' component={Search} />
+        </Tab.Navigator>
+    );
+};
 
 export default Tabs;
